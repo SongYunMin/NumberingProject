3c0bcda92069ff13006aa6056a19df8c84f5bc30
YunMin-Song <thd930308@naver.com>
2020-10-14 01:51:07 +0900

    - Creating DataBase Insert Module - Install mysql Package - Install express Package - Install ejs Package

--- MainPage.js
+++ MainPage.js
@@ -1,3 +1,33 @@
-var sql = require('mysql');
+const mysql = require('mysql');
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const router = express.Router();
 
 console.log("test Print");
+
+let connection = mysql.createConnection({
+    host : 'localhost',
+    port : '3306',
+    user : 'root',
+    password : '',
+    database : 'numbering'
+});
+// TODO : 변수로써 sql에 삽입할 수 있어야 함
+connection.connect(function(err){
+    if(err) throw err;
+    console.log("Connected Success");
+    let sql = "INSERT INTO student(name, student_num) VALUES ('Park','Sheep')";
+    connection.query(sql,function(err,result){
+        if(err)throw err;
+        console.log("Insert OK");
+    })
+});
+
+
+
+//
+// router.get('/', function(req,res){
+//     console.log("get Join URL");
+//     res.sendFile(path.join(__dirname,"../../source/index.html"));
+// });
